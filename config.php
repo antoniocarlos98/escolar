be7f3c1a3940c10c580765b01fe9d5f947a097de
João Pedro <jp3859865@gmail.com>
2023-08-02 19:52:11 -0300

    Projeto EduSis

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome_escola = "Cesar Pinheiro";
+$url = "http://localhost/escolar/";
+$endereco_escola = "Mirasselvas";
+$telefone_escola = "(91)98341-8008";
+$email_adm = "antoniocarlos0724@gmail.com";
+$rodape_relatorios = "Desenvolvido por Antonio Carlos e Joao Pedro";
+
+//variaveis do Banco de Dados local
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'sistema_escolar';
+
+?>
