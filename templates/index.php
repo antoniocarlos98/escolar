808c15375717dbad61346251d12d6dc2ed9a591e
João Pedro <jp3859865@gmail.com>
2023-08-07 13:38:52 -0300

    EduSis

--- templates/index.php
+++ templates/index.php
@@ -14,10 +14,11 @@
     <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <script src="../vendor/jquery/jquery.min.js"></script>
     <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <link rel="shortcut icon" href="../../img/favicon0.ico" type="image/x-icon">
-    <link rel="icon" href="../../img/favicon0.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/LOGO.png" type="image/png">
+
 </head>
 
+<!-- VIEWS -->
 <body id="page-top">
 
     <div id="wrapper">
@@ -37,6 +38,7 @@
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
+
                         <a class="collapse-item" href="index.php?pag=secretarios">Secretários</a>
                         <a class="collapse-item" href="index.php?pag=Professores">Professores</a>
                     </div>
@@ -165,11 +167,11 @@
 
             <div class="container-fluid">
 
-                <!-- Paginas -->
+                <!-- Paginas URLS -->
                 <?php
                 $pag = @$_GET["pag"];
                 if ($pag == null) {
-                    
+
                 } elseif ($pag == "secretarios") {
                     include_once("secretarios.php");
                 } elseif ($pag == "Professores") {
