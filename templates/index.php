5c36f3bb663e7949f0acf1c191a0a0ed6dfea7f9
antoniocarlos98 <antoniocarlos0724@gmail.com>
2023-09-01 11:22:15 -0300

    ALTERANDO

--- templates/index.php
+++ templates/index.php
@@ -205,6 +205,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
     <script src="../js/mascaras.js"></script>
     
+    
 </body>
 
 </html>
