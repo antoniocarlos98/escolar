04dcf5f6cd59ad4ce83a4f92268f41c9b1537922
antoniocarlos98 <antoniocarlos0724@gmail.com>
2023-09-18 20:42:31 -0300

    alteração

--- templates/index.php
+++ templates/index.php
@@ -34,12 +34,12 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-users"></i>
-                    <span>Pessoas</span>
+                    <span>Docentes</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
 
-                        <a class="collapse-item" href="index.php?pag=secretarios">Secretários</a>
+                        <a class="collapse-item" href="index.php?pag=secretarios">Coordenadores</a>
                         <a class="collapse-item" href="index.php?pag=Professores">Professores</a>
                     </div>
                 </div>
@@ -49,13 +49,13 @@
            <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-home"></i>
-                    <span>Pessoas II</span>
+                    <span>Colaboradores e Alunos</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Dados XX:</h6>
                         <a class="collapse-item" href="index.php?pag=alunos">Alunos</a>
-                        <a class="collapse-item" href="index.php?pag=menu4">Menu 4</a>
+                        <a class="collapse-item" href="index.php?pag=menu4">Equipe de apoio</a>
                         <a class="collapse-item" href="index.php?pag=menu5">Menu 5</a>
                     </div>
                 </div>
